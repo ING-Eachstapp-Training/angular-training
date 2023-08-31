c7e9eed4fe6de04b412df136b93e81f845a395bb
Yunus Ozturk <105066488+yunus-stapp@users.noreply.github.com>
2023-08-31 14:22:11 +0200

    Update index.ts

--- chapter-1/index.ts
+++ chapter-1/index.ts
@@ -3,57 +3,64 @@ interface Todo {
   isChecked: boolean;
 }
 
-const todos: Todo[] = [];
+class TodoList {
+  todos: Todo[];
 
-function readTodos() {
-  console.log("Todo List:");
-  todos.forEach((todo, index) => {
-    console.log(`${index + 1}. [${todo.isChecked ? "x" : " "}] ${todo.title}`);
-  });
-}
+  constructor() {
+    this.todos = [];
+  }
 
-function createTodo(title: string) {
-  const newTodo: Todo = {
-    title,
-    isChecked: false,
-  };
-  todos.push(newTodo);
-  console.log("New todo created:", title);
-}
+  readTodos(): void {
+    console.log("Todo List:");
+    this.todos.forEach((todo, index) => {
+      console.log(
+        `${index + 1}. [${todo.isChecked ? "x" : " "}] ${todo.title}`
+      );
+    });
+  }
 
-function deleteTodo(id: number) {
-  if (id >= 1 && id <= todos.length) {
-    todos.splice(id - 1, 1);
-    console.log("Todo deleted.");
-  } else {
-    console.log("Invalid todo ID.");
+  createTodo(title: string): void {
+    const newTodo: Todo = { title: title, isChecked: false };
+    this.todos.push(newTodo);
+    console.log("New todo created:", title);
+  }
+
+  deleteTodo(id: number): void {
+    if (id >= 1 && id <= this.todos.length) {
+      this.todos.splice(id - 1, 1);
+      console.log("Todo deleted.");
+    } else {
+      console.log("Invalid todo ID.");
+    }
+  }
+
+  checkTodo(id: number): void {
+    if (id >= 1 && id <= this.todos.length) {
+      this.todos[id - 1].isChecked = true;
+      console.log("Todo checked off.");
+    } else {
+      console.log("Invalid todo ID.");
+    }
+  }
+
+  uncheckTodo(id: number): void {
+    if (id >= 1 && id <= this.todos.length) {
+      this.todos[id - 1].isChecked = false;
+      console.log("Todo unchecked.");
+    } else {
+      console.log("Invalid todo ID.");
+    }
   }
 }
 
-function checkTodo(id: number) {
-  if (id >= 1 && id <= todos.length) {
-    todos[id - 1].isChecked = true;
-    console.log("Todo checked off.");
-  } else {
-    console.log("Invalid todo ID.");
-  }
-}
+const todoList = new TodoList();
 
-function uncheckTodo(id: number) {
-  if (id >= 1 && id <= todos.length) {
-    todos[id - 1].isChecked = false;
-    console.log("Todo unchecked.");
-  } else {
-    console.log("Invalid todo ID.");
-  }
-}
-
-createTodo("Buy groceries");
-createTodo("Finish homework");
-readTodos();
-checkTodo(1);
-readTodos();
-uncheckTodo(1);
-readTodos();
-deleteTodo(1);
-readTodos();
+todoList.createTodo("Buy groceries");
+todoList.createTodo("Finish homework");
+todoList.readTodos();
+todoList.checkTodo(1);
+todoList.readTodos();
+todoList.uncheckTodo(1);
+todoList.readTodos();
+todoList.deleteTodo(1);
+todoList.readTodos();
