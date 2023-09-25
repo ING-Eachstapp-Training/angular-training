0f5038d94f4fab704337cf8d62498d55cf130fb4
Yunus <yunus.ozturk@eachstapp.com>
2023-09-25 11:14:58 +0200

    change folder structure

--- chapter-4/src/app/app-routing.module.ts
+++ chapter-4/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.component';
-import { TodoDetailPageComponent } from './pages/todo-detail-page/todo-detail-page.component';
+import { TodoListPageComponent } from './pages/todo-parent-page/todo-list-page/todo-list-page.component';
+import { TodoDetailPageComponent } from './pages/todo-parent-page/todo-detail-page/todo-detail-page.component';
 import { TodoParentPageComponent } from './pages/todo-parent-page/todo-parent-page.component';
 
 const routes: Routes = [
--- chapter-4/src/app/app.module.ts
+++ chapter-4/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { TodoListPageComponent } from './pages/todo-list-page/todo-list-page.component';
-import { TodoDetailPageComponent } from './pages/todo-detail-page/todo-detail-page.component';
+import { TodoListPageComponent } from './pages/todo-parent-page/todo-list-page/todo-list-page.component';
+import { TodoDetailPageComponent } from './pages/todo-parent-page/todo-detail-page/todo-detail-page.component';
 import { TodoParentPageComponent } from './pages/todo-parent-page/todo-parent-page.component';
 
 @NgModule({
